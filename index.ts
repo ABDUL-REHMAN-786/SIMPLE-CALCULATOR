6ece58de36a001f705166fd270e35f972ff1d01b
ABDUL REHMAN <160714317+ABDUL-REHMAN-786@users.noreply.github.com>
2024-03-17 15:17:59 +0500

    frist commit simple calculator

---
+++ index.ts
@@ -0,0 +1,18 @@
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+    {message: "enter frist number", type: "number", name: "fristnumber"},
+    {message: "enter second number", type: "number", name: "secondnumber"},
+    {message: "select operator", type: "list", name: "operator", choices: ["addition", "subtraction", "multiplication", "division"],}
+]);
+
+// conditional statement
+if (answer.operator === "addition")
+{console.log(answer.fristnumber + answer.secondnumber);}
+else if (answer.operator === "subtraction")
+{console.log(answer.fristnumber - answer.secondnumber);}
+else if (answer.operator === "multiplication")
+{console.log(answer.fristnumber * answer.secondnumber);}
+else if (answer.operator === "division")
+{console.log(answer.fristnumber / answer.secondnumber);}
+else {console.log("please select valid operator")}
